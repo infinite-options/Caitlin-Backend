6bfefe5a4f633732232297eacb5814db18907207
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-02-05 06:20:22 -0800

    loading functions added

--- functions/index.js
+++ functions/index.js
@@ -27,7 +27,7 @@ exports.SetUserInfo = functions.https.onRequest(async (req, res) => {
 
 exports.CompleteTaskStep = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
-  const userId = '7R6hAVmDrNutRkG3sVRy'
+  const userId = req.get('userId').toString()
   const routineId = req.get('routineId').toString()
   const taskId = req.get('taskId').toString()
   const stepNumber = parseInt(req.get('stepNumber'))
@@ -42,25 +42,6 @@ exports.CompleteTaskStep = functions.https.onRequest(async (req, res) => {
         steps.steps[stepNumber].status = 'Complete'
 
         let setStepComplete = task.set(steps);
-      }
-      return routineId;
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
-
-exports.CompleteTaskStep = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const userId = '7R6hAVmDrNutRkG3sVRy'
-
-  let task = db.collection('users').doc(userId)
-  let getTaskDoc = task.get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log('No such document!');
-      } else {
-        var steps = doc.data();
-        // Stuff Here
       }
       return routineId;
     })
@@ -117,4 +98,233 @@ exports.CompleteTaskStep = functions.https.onRequest(async (req, res) => {
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
   res.redirect(303, "success");
+});
+
+exports.copyRoutineDoc = functions.https.onRequest(async (req, res) => {
+  const userId = req.get('userId').toString()
+  const routineId1 = req.get('routineId1').toString()
+  const routineId2 = req.get('routineId2').toString()
+
+  var docData
+
+  let doc1 = db.collection('users').doc(userId).collection('routines').doc(routineId1)
+  let getDoc1 = doc1.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        docData = doc.data()
+        console.log('docData: ' + docData)
+      }
+      return docData;
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+
+  let doc2 = db.collection('users').doc(userId).collection('goals&routines').doc(routineId2)
+  let getDoc2 = doc2.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        doc2.set(docData)
+      }
+      return docData;
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+    res.redirect(303, "success");
+});
+
+exports.copyTaskDoc = functions.https.onRequest(async (req, res) => {
+  const userId = req.get('userId').toString()
+  const routineId1 = req.get('routineId1').toString()
+  const routineId2 = req.get('routineId2').toString()
+  const taskId1 = req.get('taskId1').toString()
+  const taskId2 = req.get('taskId2').toString()
+
+  var docData
+
+  let doc1 = db.collection('users').doc(userId).collection('routines').doc(routineId1).collection('tasks').doc(taskId1)
+  let getDoc1 = doc1.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        docData = doc.data()
+        console.log('docData: ' + docData)
+      }
+      return docData;
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+
+  let doc2 = db.collection('users').doc(userId).collection('goals&routines').doc(routineId2).collection('actions&tasks').doc(taskId2)
+  let getDoc2 = doc2.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        doc2.set(docData)
+      }
+      return docData;
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+    res.redirect(303, "success");
+});
+
+exports.renameTasksArray = functions.https.onRequest(async (req, res) => {
+  const userId = req.get('userId').toString()
+  const routineId1 = req.get('routineId1').toString()
+
+  let doc1 = db.collection('users').doc(userId).collection('goals&routines').doc(routineId1)
+  let getDoc1 = doc1.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        var tasks = doc.data();
+        let newNameData =
+        {
+          'actions&tasks': tasks.tasks
+        }
+
+        doc1.set(newNameData)
+
+      }
+      return doc.data();
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+    res.redirect(303, "success");
+});
+
+exports.loadGoalsAndRoutines = functions.https.onRequest(async (req, res) => {
+  const userId = req.get('userId').toString()
+  const routineId = req.get('routineId').toString()
+  const routineTitle = req.get('routineTitle').toString()
+  const category = req.get('category').toString()
+  var is_persistent = (category === 'Routine') ? true : false
+  const photo = req.get('photo').toString()
+
+
+  let userDoc = db.collection('users').doc(userId)
+  let getUserDoc = userDoc.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        var goalsAndRoutines = doc.data();
+        let item =
+        {
+          'id':  routineId,
+          'title':  routineTitle,
+          'audio': '',
+          'available_end_time': '23:59:59',
+          'available_start_time': '00:00:00',
+          'category': category,
+          'datetime_completed': '2020/02/03T08:32:42',
+          'datetime_started': '2020/02/03T07:16:32',
+          'is_available': true,
+          'is_complete': false,
+          'is_persistent': is_persistent,
+          'is_timed': true,
+          'notifies_ta': true,
+          'photo': photo,
+          'remind_types': ['push', 'text'],
+          'reminds_user': true,
+          'ta_notification_reasons': ['overtime','start','end'],
+          'tags': ['morning', 'preparation']
+        }
+
+        goalsAndRoutines['goals&routines'].push(item)
+        userDoc.set(goalsAndRoutines)
+
+      }
+      return doc.data();
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+    res.redirect(303, "success");
+});
+
+exports.loadActionsAndtasks = functions.https.onRequest(async (req, res) => {
+  const userId = req.get('userId').toString()
+  const routineId = req.get('routineId').toString()
+  const taskId = req.get('taskId').toString()
+  const taskTitle = req.get('taskTitle').toString()
+  const photo = req.get('photo').toString()
+
+
+  let routineDoc = db.collection('users').doc(userId).collection('goals&routines').doc(routineId)
+  let getRoutineDoc = routineDoc.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        var actionsAndtasks = doc.data();
+        let item =
+        {
+          'id':  taskId,
+          'title':  taskTitle,
+          'audio': '',
+          'available_end_time': '23:59:59',
+          'available_start_time': '00:00:00',
+          'datetime_completed': '2020/02/03T08:32:42',
+          'datetime_started': '2020/02/03T07:16:32',
+          'is_available': true,
+          'is_complete': false,
+          'is_timed': true,
+          'notifies_ta': true,
+          'photo': photo,
+          'remind_types': ['push', 'text'],
+          'reminds_user': true,
+          'ta_notification_reasons': ['overtime','start','end'],
+          'tags': ['morning', 'preparation']
+        }
+
+        actionsAndtasks['actions&tasks'].push(item)
+        routineDoc.set(actionsAndtasks)
+
+      }
+      return doc.data();
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+    res.redirect(303, "success");
+});
+
+exports.renameStepsArray = functions.https.onRequest(async (req, res) => {
+  const userId = req.get('userId').toString()
+  const routineId1 = req.get('routineId1').toString()
+  const taskId1 = req.get('taskId1').toString()
+
+  let doc1 = db.collection('users').doc(userId).collection('goals&routines').doc(routineId1).collection('actions&tasks').doc(taskId1)
+  let getDoc1 = doc1.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        var steps = doc.data();
+        let newNameData =
+        {
+          'instructions&steps': steps.steps
+        }
+        doc1.set(newNameData)
+
+      }
+      return doc.data();
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+    res.redirect(303, "success");
 });
