b414d686d57ea7d6ae18daa0e0d3c35b051f37e7
Xiaosui Zhang <maxchang39@yahoo.com>
2020-07-15 13:31:53 -0700

    add shortcuts for switching between two dbs

---
+++ MovetoMyLife.sh
@@ -0,0 +1,2 @@
+cp '.runtimeconfig(MyLife DB).json' .runtimeconfig.json
+cp ServiceAccountMyLife.json ServiceAccountKey.json
---
+++ MovetoMySpace.sh
@@ -0,0 +1,2 @@
+cp '.runtimeconfig(MySpace DB).json' .runtimeconfig.json
+cp ServiceAccountMySpace.json ServiceAccountKey.json
